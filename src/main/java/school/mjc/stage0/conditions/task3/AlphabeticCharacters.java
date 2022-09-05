d01c0ca39bbdf24243d5e1bdae79ac6fd4be271d
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 00:07:38 +0300

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z'))){
+                System.out.println("wrong alphabet!");
+            } else {
+                if (character == 'a' || character == 'e' ||
+                        character == 'o' || character == 'i' ||
+                        character == 'u' || character == 'y'
+                || character == 'A' || character == 'E' ||
+                        character == 'O' || character == 'I' ||
+                        character == 'U' || character == 'Y') {
+                    System.out.println("Vowel");
+                } else {
+                    System.out.println("Consonant");
+                }
+            }
     }
 }
