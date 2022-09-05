740fcf01294b95e8d505fbb18cbed9ab6ffe4703
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 00:12:26 +0300

    Update DaysInMonthPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month % 2 == 1) System.out.println(31);
+            else if (month == 2) System.out.println(28);
+            else System.out.println(30);
     }
 }
