65dae2cd6cb8821fb0eb7805355c5d93b8307915
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-05 23:33:57 +0300

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if (!(month >= 1 && month <= 12)) System.out.println("wrong number!");
+            if (month >= 9 && month <= 11) System.out.println("Autumn");
+            if (month == 12 || month == 1 || month == 2) System.out.println("Winter");
+            if (month >= 3 && month <= 5) System.out.println("Spring");
+            if (month >= 6 && month <= 8) System.out.println("Summer");
 
     }
 }
