1d467802b40e36080046673f5abd0a78b14aedc4
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-05 23:47:04 +0300

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000) {
+                        System.out.println((double)salary * 0.85);
+                    }
+                    else if (10000 < salary && salary <= 20000) {
+                        System.out.println((double)salary * 0.82);
+                    }
+                    else if (salary > 20000) {
+                        System.out.println((double)salary * 0.8);
+                    }
+                    else if (salary < 0) {
+                        System.out.println("wrong input!");
+                    }
     }
 }
